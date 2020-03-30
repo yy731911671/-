dbd57bf883b9ff4df634f30c0bdcad4072a0f1a9
Yangyi <yangyionly@qq.com>
2020-03-30 20:36:41 +0800

    finish homework-3

--- 第三次作业/load_tiles.py
+++ 第三次作业/load_tiles.py
@@ -4,8 +4,8 @@ from pygame import image, Rect, Surface
 TILE_POSITIONS = [
     ('#',0,0),
     (' ',0,1),
-    ('.',3,0),
-    ('@',2,0)
+    ('@',3,0),
+    ('.',2,0)
 ]
 
 SIZE = 32
@@ -20,11 +20,18 @@ def load_tiles():
         tiles[symbol] = rect
     return tile_img, tiles
 
+def get_tile_rect(x, y):
+    return(x * SIZE, y * SIZE)
+
+
+
+
+
 if __name__ == '__main__':
     tile_img, tiles = load_tiles()
     m = Surface((32*4,32))   #width, height
     m.blit(tile_img.subsurface(tiles['#']),(0,0))
     m.blit(tile_img.subsurface(tiles[' ']),(32,0))
-    m.blit(tile_img.subsurface(tiles['.']),(64,0))
-    m.blit(tile_img.subsurface(tiles['@']),(96,0))
+    m.blit(tile_img.subsurface(tiles['@']),(64,0))
+    m.blit(tile_img.subsurface(tiles['.']),(96,0))
     image.save(m,'tile_combo.png')
